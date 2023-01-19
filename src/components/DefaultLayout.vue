d587e67d1d831b7fcc5cf22db40fc037f9e8e4df
Emma <emma.tangg@gmail.com>
2023-01-19 15:10:11 -0500

    Add files via upload

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <navbar />
+  <main>
+    <div class="max-w-[1200px] x-auto">
+      <router-view />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue'
+</script>
